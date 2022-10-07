6bd2192059e05c2bee044482a8276ffea0ea4d62
ahidulislam1994 <ahidulislam1994@gmail.com>
2022-10-07 23:54:39 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+{/* <Script>
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname){
+    for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    even.CurrentTarget.classList.add("active-link");
+    getElementsByClassName("tabname");
+}
+
+</Script> */}
